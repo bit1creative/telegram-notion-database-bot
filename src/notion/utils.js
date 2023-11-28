96cd0eb1fa7556ece69dd34642b1cdaadbbcd7d2
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-11-28 12:24:53 +0200

    use local user

--- src/notion/utils.js
+++ src/notion/utils.js
@@ -1,6 +1,7 @@
 import Fuse from "fuse.js";
 // import { DateTime } from "luxon";
 import { notionClient } from "./index.js";
+import { USERS } from "../telegram-bot/constants.js";
 
 export const findDatabase = async (databaseId) => {
   const databases = await notionClient.search({
@@ -27,13 +28,18 @@ export const createNotionDatabaseEntry = async (
 
   // const parsedEffort = effortFuse.search(effort)[0].item.name;
   // const parsedDate = DateTime.fromFormat(date.trim(), "dd/MM/yyyy").toISO();
-  const parsedLead = await findUser(lead);
+  // const parsedLead = await findUser(lead);
+  const localUser = findLocalUser(lead);
 
   const pageOptions = {
     properties: {
       Name: {
         type: "title",
         title: [{ type: "text", text: { content: event } }],
+      },
+      "Person bot": {
+        type: "text",
+        text: [{ type: "text", text: { content: localUser } }],
       },
       // Status: {
       //   type: "select",
@@ -53,19 +59,29 @@ export const createNotionDatabaseEntry = async (
       //     name: parsedEffort,
       //   },
       // },
-      Lead: {
-        type: "people",
-        people: [
-          {
-            object: "user",
-            id: parsedLead.id,
-          },
-        ],
-      },
+      // Lead: {
+      //   type: "people",
+      //   people: [
+      //     {
+      //       object: "user",
+      //       id: parsedLead.id,
+      //     },
+      //   ],
+      // },
     },
   };
 
   return pageOptions;
+};
+
+export const findLocalUser = (userName) => {
+  const userFuse = new Fuse(USERS, {
+    keys: ["name"],
+    threshold: 0.8,
+  });
+
+  const user = userFuse.search(userName)[0].item;
+  return user;
 };
 
 export const findUser = async (userName) => {
---
+++ src/telegram-bot/constants.js
@@ -0,0 +1,17 @@
+export const USERS = [
+  "Andriy S",
+  "Dmytro F",
+  "Ivan M",
+  "Lina I",
+  "Oksana V",
+  "Petro  Y",
+  "Severyn Y",
+  "Slavik Y",
+  "Taras Y",
+  "Yulia B",
+  "Bodgan V",
+  "Nazar B",
+  "Mykola",
+  "Mark",
+  "Ilya",
+];
